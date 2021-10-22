2d3b651ea848bc982ec2fe6ad9f47819445f4667
izacpeterson <izacpeterson@gmail.com>
2021-10-22 17:31:11 -0600

    lots of work

--- index.js
+++ index.js
@@ -2,17 +2,36 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 
+app.use(express.json());
+
 app.get("/api/getmovies", (req, res) => {
   let userEmail = req.query.user;
-  let movies = JSON.parse(fs.readFileSync("./data/data.json"));
-  movies.userData.forEach((user) => {
-    if (user.email === userEmail) {
-      res.send(user);
-    } else {
-      res.send("error");
-    }
-    console.log(user);
-  });
+  let data = JSON.parse(fs.readFileSync("./data/data.json"));
+  let index = data.userData.map((user) => user.email).indexOf(userEmail);
+  res.send(data.userData[index] || "error");
+});
+
+app.post("/api/addmovie", (req, res) => {
+  let userEmail = req.body.email;
+  let movie = req.body.movie;
+  let data = JSON.parse(fs.readFileSync("./data/data.json"));
+  let index = data.userData.map((user) => user.email).indexOf(userEmail);
+  data.userData[index].movies.push(movie);
+  fs.writeFileSync("./data/data.json", JSON.stringify(data));
+  res.send("movie added");
+});
+
+app.post("/api/deletemovie", (req, res) => {
+  let userEmail = req.body.email;
+  let movie = req.body.movie;
+  let data = JSON.parse(fs.readFileSync("./data/data.json"));
+  let index = data.userData.map((user) => user.email).indexOf(userEmail);
+  let movIndex = data.userData[index].movies.indexOf(movie);
+  let movList = data.userData[index].movies;
+  movList.splice(movIndex, 1);
+  data.userData[index].movies = movList;
+  fs.writeFileSync("./data/data.json", JSON.stringify(data));
+  res.send("deleted");
 });
 
 app.use(express.static("public"));
--- public/app.js
+++ public/app.js
@@ -1,28 +1,128 @@
+//Page Header
 Vue.component("pageheader", {
+  props: ["user"],
   template: `
   <header>
     <h1>Movie List</h1>
+    <h2>{{user}}</h2>
     <button>Account</button>
   </header>`,
 });
-Vue.component("username", {
-  props: ["email"],
-  template: `<h2>User: {{email}}</h2>`,
-});
 
-let app = new Vue({
-  el: "#app",
-  data: {
-    email: "",
+//List of movies
+Vue.component("movielist", {
+  props: {
+    user: { type: String, required: true },
+    movies: { type: Array, required: true },
   },
+  template: `<ul class="movieList"><li v-for="movie in movies" class="movie"><h2 class="movieName">{{movie}}</h2><button class="delete" v-on:click="deleteMovie(movie)">Del</button></li></ul>`,
+
   methods: {
-    getUser: () => {
-      fetch("/api/getmovies?user=izacpeterson@gmail.com")
-        .then((raw) => raw.json())
-        .then((data) => {
-          console.log(data);
-          return data.email;
-        });
+    deleteMovie(movietitle) {
+      this.$emit("delMov", movietitle);
+      console.log(movietitle);
+      var myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+      var raw = JSON.stringify({
+        email: this.user,
+        movie: movietitle,
+      });
+      var requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow",
+      };
+      fetch("/api/deletemovie", requestOptions)
+        .then((response) => response.text())
+        .then((result) => console.log(result))
+        .catch((error) => console.log("error", error));
     },
   },
 });
+
+//Add a movie
+Vue.component("addmovie", {
+  props: {
+    user: { type: String, required: true },
+  },
+  template: `<div class="addMov"><input v-model="movieTitle" id="newMovieName" placeholder="Movie Name"><button @click="submit">Add</button></div>`,
+  data() {
+    return {
+      movieTitle: "",
+    };
+  },
+  methods: {
+    submit: function () {
+      this.$emit("addMov", document.getElementById("newMovieName").value);
+      var myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+
+      var raw = JSON.stringify({
+        email: this.user,
+        movie: document.getElementById("newMovieName").value,
+      });
+
+      var requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow",
+      };
+
+      fetch("/api/addmovie", requestOptions)
+        .then((response) => response.text())
+        .then((result) => console.log(result))
+        .catch((error) => console.log(error));
+      document.getElementById("newMovieName").value = "";
+      document.getElementById("newMovieName").focus();
+    },
+  },
+});
+
+//Vue App
+let app = new Vue({
+  el: "#app",
+  template: `<div class="app">
+      <pageheader v-bind:user="user"></pageheader>
+      <movielist v-bind:user="user" v-bind:movies="movies" @delMov="removeMovFromList($event)"></movielist>
+      <addmovie v-bind:user="user" @addMov="addMovtoList($event)"></addmovie>
+      </div>`,
+  data: {
+    user:
+      new URLSearchParams(window.location.search).get("username") ||
+      localStorage.getItem("username"),
+    movies: [],
+  },
+  created() {
+    this.getMovies();
+  },
+  methods: {
+    getMovies() {
+      fetch("/api/getmovies?user=" + this.user)
+        .then((raw) => raw.json())
+        .then((data) => {
+          this.movies = data.movies;
+        });
+    },
+    addMovtoList(newMov) {
+      this.movies.push(newMov);
+      console.log("newMOV", this.movies);
+    },
+    removeMovFromList(mov) {
+      const index = this.movies.indexOf(mov);
+      this.movies.splice(index, 1);
+    },
+  },
+});
+
+const urlParams = new URLSearchParams(window.location.search);
+const myParam = urlParams.get("username") || localStorage.getItem("username");
+const userName = localStorage.getItem("username");
+if (myParam) {
+  localStorage.setItem("username", myParam);
+  console.log(myParam);
+}
+if (!myParam) {
+  window.location = "/login";
+}
